b975bf313c38d5c63a088b0e647e652c98f5cc66
עידו פרלמוטר (Ido Perlmuter) <ido@ido50.net>
2013-01-15 19:52:08 +0000

    Import of IDOPEREL/Brannigan-1.0 from CPAN.
    gitpan-cpan-distribution: Brannigan
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         IDOPEREL/Brannigan-1.0.tar.gz
    gitpan-cpan-author:       IDOPEREL
    gitpan-cpan-maturity:     released

--- lib/Brannigan/Tree.pm
+++ lib/Brannigan/Tree.pm
@@ -1,6 +1,6 @@
 package Brannigan::Tree;
 
-our $VERSION = "0.9";
+our $VERSION = "1.0";
 $VERSION = eval $VERSION;
 
 use strict;
@@ -13,7 +13,7 @@ Brannigan::Tree - A Brannigan validation/parsing scheme tree, possibly built fro
 
 =head1 VERSION
 
-version 0.9
+version 1.0
 
 =head1 DESCRIPTION
 
@@ -468,7 +468,7 @@ L<http://search.cpan.org/dist/Brannigan/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2010 Ido Perlmuter.
+Copyright 2010-2013 Ido Perlmuter.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
